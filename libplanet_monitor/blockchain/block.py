17b3052a93f610441d2a5cf32a39043dce797656
moreal <dev.moreal@gmail.com>
2020-02-16 19:07:46 +0900

    feat(#1): implement serialization

--- libplanet_monitor/blockchain/block.py
+++ libplanet_monitor/blockchain/block.py
@@ -1,9 +1,22 @@
 from dataclasses import dataclass, field
-from typing import List, AnyStr, Optional
+from typing import AnyStr, Dict, List, Optional
+
+from bencodex import BValue, dumps, loads
 
 from .address import Address
 from .hashcash import Hash, Nonce
 from .transaction import Transaction
+
+
+_INDEX_KEY = b'i'
+_TIMESTAMP_KEY = b't'
+_DIFFICULTY_KEY = b'd'
+_NONCE_KEY = b'n'
+_MINER_KEY = b'm'
+_PREVIOUS_HASH_KEY = b'p'
+_HASH_KEY = b'h'
+_BLOCK_HEADER_KEY = 'H'
+_TRANSACTIONS_KEY = b'T'
 
 
 @dataclass(frozen=True)
@@ -17,3 +30,36 @@ class Block:
     timestamp: str
     transactions: List[Transaction]
     previous_hash: Optional[Hash]
+
+    def serialize(self) -> bytes:
+        return dumps(self.bencode())
+
+    def bencode(self) -> BValue:
+        return {
+            _BLOCK_HEADER_KEY: {
+                _INDEX_KEY: self.index,
+                _TIMESTAMP_KEY: self.timestamp,
+                _DIFFICULTY_KEY: self.difficulty,
+                _NONCE_KEY: self.nonce,
+                _MINER_KEY: self.miner,
+                _PREVIOUS_HASH_KEY: self.previous_hash,
+                _HASH_KEY: self.hash,
+            },
+            _TRANSACTIONS_KEY: self.transactions
+        }
+
+    @staticmethod
+    def deserialize(raw: bytes) -> Block:
+        decoded = loads(raw)
+        block_header = decoded[_BLOCK_HEADER_KEY]
+        transactions = list(map(Transaction.from_dict, decoded[_TRANSACTIONS_KEY]))
+        return Block(
+            index=block_header[_INDEX_KEY],
+            timestamp=block_header[_TIMESTAMP_KEY],
+            difficulty=block_header[_DIFFICULTY_KEY],
+            nonce=block_header[_NONCE_KEY],
+            miner=block_header[_MINER_KEY],
+            previous_hash=block_header[_PREVIOUS_HASH_KEY],
+            hash=block_header[_HASH_KEY],
+            transactions=transactions
+        )
--- libplanet_monitor/blockchain/transaction.py
+++ libplanet_monitor/blockchain/transaction.py
@@ -1,10 +1,22 @@
 from dataclasses import dataclass
-from typing import AnyStr, NewType, Sequence
+from typing import List, NewType
+
+from bencodex import BValue, dumps, loads
 
 from .address import Address
 from .crypto import PublicKey
 
+
+_NONCE_KEY = b'n'
+_SIGNER_KEY = b's'
+_UPDATED_ADDRESSES_KEY = b'u'
+_PUBLIC_KEY_KEY = b'p'
+_TIMESTAMP_KEY = b't'
+_ACTIONS_KEY = b'a'
+_SIGNATURE_KEY = b'S'
+
 Signature = NewType('Signature', bytes)
+
 
 @dataclass(frozen=True)
 class Transaction:
@@ -14,4 +26,36 @@ class Transaction:
     public_key: PublicKey
     signature: Signature
     timestamp: str
-    updatedAddresses: Sequence[Address]
+    updated_addresses: List[Address]
+    actions: List[dict]
+
+    def serialize(self) -> bytes:
+        return dumps(self.bencode())
+
+    def bencode(self) -> BValue:
+        return {
+            _NONCE_KEY: self.nonce,
+            _SIGNER_KEY: self.signer,
+            _PUBLIC_KEY_KEY: self.public_key,
+            _SIGNATURE_KEY: self.signature,
+            _TIMESTAMP_KEY: self.timestamp,
+            _UPDATED_ADDRESSES_KEY: self.updated_addresses,
+            _ACTIONS_KEY: self.actions,
+        }
+
+    @staticmethod
+    def deserialize(raw: bytes) -> Transaction:
+        decoded = loads(raw)
+        return Transaction.from_dict(decoded)
+
+    @staticmethod
+    def from_dict(raw: dict) -> Transaction:
+        return Transaction(
+            nonce=raw[_NONCE_KEY],
+            signer=raw[_SIGNER_KEY],
+            public_key=raw[_PUBLIC_KEY_KEY],
+            signature=raw[_SIGNATURE_KEY],
+            timestamp=raw[_TIMESTAMP_KEY],
+            updated_addresses=raw[_UPDATED_ADDRESSES_KEY],
+            actions=raw[_ACTIONS_KEY],
+        )
